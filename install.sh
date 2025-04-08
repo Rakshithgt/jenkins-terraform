b12467e718732ac41fe6f785ad901a04a35ecac0
Rakshithgt <114352972+Rakshithgt@users.noreply.github.com>
2025-04-08 23:33:20 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+set -e
+exec >> /var/log/init-script.log 2>&1
+
+echo "starting initialization script..."
+
 sudo apt update -y
 wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc
 echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
@@ -27,3 +33,71 @@ wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dear
 echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
 sudo apt-get update
 sudo apt-get install trivy -y
+
+# Install AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip -y
+unzip awscliv2.zip
+sudo ./aws/install
+
+# Install Kubectl
+sudo apt update
+sudo apt install curl -y
+sudo curl -LO "https://dl.k8s.io/release/v1.28.4/bin/linux/amd64/kubectl"
+sudo chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+kubectl version --client
+
+# Install eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
+
+# Install Terraform
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt install terraform -y
+
+
+echo "Initialization script completed successfully."
+
+echo "======================================="
+echo "Initialization Script Completed"
+echo "Checking service statuses..."
+echo "---------------------------------------"
+
+# Jenkins
+echo "[*] Jenkins Status:"
+sudo systemctl is-active --quiet jenkins && echo "Jenkins is running" || echo "Jenkins is NOT running "
+
+# Docker
+echo "[*] Docker Status:"
+sudo systemctl is-active --quiet docker && echo "Docker is running" || echo "Docker is NOT running"
+
+# SonarQube
+echo "[*] SonarQube Container:"
+docker ps | grep sonar && echo "SonarQube is running" || echo "SonarQube container is NOT running"
+
+# Trivy
+echo "[*] Trivy Version:"
+trivy --version
+
+# AWS CLI
+echo "[*] AWS CLI Version:"
+aws --version
+
+# Kubectl
+echo "[*] Kubectl Version:"
+kubectl version --client --short
+
+# eksctl
+echo "[*] eksctl Version:"
+eksctl version
+
+#terraform
+echo "[*] terraform Version:"
+terraform --version
+
+echo "======================================="
+echo  "All services initialized (check above for any)"
